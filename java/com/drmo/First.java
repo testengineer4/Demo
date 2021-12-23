85693f7e1b1b37f47795570ace874cf1d0d743b4
venumadhav bollam <seniortestengineer4@gmail.com>
2021-12-23 08:32:53 +0530

    this my first git poc

---
+++ java/com/drmo/First.java
@@ -0,0 +1,34 @@
+package com.drmo;
+
+public class First {
+	int k;
+	
+	First l;
+	 static int m=100;
+	 
+	 
+	int[] a= {35, 67,78, 99};
+	//			0 1  2 3   a[0]
+	//[35, 67,78, 99,]
+	 int method()  
+	    {    
+		 k=k+6;
+	        int n=90;
+	        
+	        System.out.println("i am from method "+n);
+	        
+	       return n=n+10;
+	    }  
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		First f=new First(); //object creation
+		
+		System.out.println(First.m);
+		System.out.println(f.a[3]);
+		
+System.out.println("i am from main method "+f.method());
+	}
+
+}
